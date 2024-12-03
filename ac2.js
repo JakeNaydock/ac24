fc9490f04d56d28a1f57bd9820eada4b7c4d32ae
Jake <jnaydockeahs@gmail.com>
2024-12-03 00:19:57 -0500

    Progress on day 2 part 1

--- ac2.js
+++ ac2.js
@@ -1,2 +1,32 @@
+const { dir } = require('console');
 const fs = require('fs');
-const data = fs.readFileSync('./input1.txt', 'utf-8');
+const data = fs.readFileSync('./input2.txt', 'utf-8');
+const arrLines = data.split('\n');
+//console.log('arrLines:\n', arrLines);
+
+for (let i = 0; i < arrLines.length; i++) {
+    const arrCurLine = arrLines[i].split(' ');
+    //console.log('arrCurLine:\n', arrCurLine);
+
+    let previousDirection = 'equal';
+    for (let j = 0; j < arrCurLine.length; j++) {
+
+        const currentNum = arrCurLine[j] * 1;
+        const previousNum = (!j) ? null : arrCurLine[j - 1] * 1;
+        console.log(`Line ${i}. Current num: ${currentNum}. Previous num: ${previousNum}`);
+        if (!previousNum) continue;
+
+        if (currentNum < previousNum) {
+            direction = 'decreasing';
+        } else if (currentNum === previousNum) {
+            direction = 'equal';
+        } else if (currentNum > previousNum) {
+            direction = 'increasing';
+        }
+
+
+
+
+    }
+
+}
