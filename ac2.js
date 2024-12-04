f28510b48ebc6768a4914970f7828362b6def754
Jake <jnaydockeahs@gmail.com>
2024-12-03 21:46:38 -0500

    Fixed an issue but still getting incorrect answer day 2 p1

--- ac2.js
+++ ac2.js
@@ -11,6 +11,10 @@ for (let i = 0; i < arrLines.length; i++) {
     let previousDirection;
     let direction;
     for (let j = 0; j < arrCurLine.length; j++) {
+        if (i < 10) {
+            console.log('j val', j);
+            console.log('Previous num condition', (j > 0) ? arrCurLine[j - 1] * 1 : null);
+        }
         const currentNum = arrCurLine[j] * 1;
         //console.log('Arr curr line length', arrCurLine.length);
 
@@ -20,24 +24,33 @@ for (let i = 0; i < arrLines.length; i++) {
 
         //If there is no previous direction stop here
         //if (!previousDirection) continue;
-        direction = (previousNum && currentNum < previousNum) ? 'decreasing' : 'increasing';
-        let intSafe = safetyCheck(currentNum, previousNum, direction, previousDirection);
-        console.log(`Safety check line ${i} char ${j}`, intSafe);
+        direction = ((previousNum) && currentNum < previousNum) ? 'decreasing' : 'increasing';
+        let intSafe = safetyCheck(currentNum, previousNum, direction, previousDirection, i);
+        if (i < 10) console.log(`Safety check line ${i} char ${j}:`, intSafe);
         if (!intSafe) break;
         if (j === arrCurLine.length - 1) safeRowCount++;
-        direction = previousDirection;
+        previousDirection = direction;
     }
 }
 
 console.log('Safe row count:', safeRowCount);
 
 
-function safetyCheck(currentNum, previousNum, direction, previousDirection) {
-    console.log(`Current num: ${currentNum}. Previous num: ${previousNum}`);
+function safetyCheck(currentNum, previousNum, direction, previousDirection, index) {
+    if (index < 10) {
+        console.log(`Current num: ${currentNum}. Previous num: ${previousNum}`);
+        console.log(`Direction: ${direction}. Previous direction: ${previousDirection}`);
+    }
+
     if (!previousNum) return 1;
+
+
     if (currentNum === previousNum) return 0;
     let absDiff = Math.abs(currentNum - previousNum);
-    console.log('Abs diff: ', absDiff);
+    if (index < 10) {
+        console.log('Abs diff: ', absDiff);
+    }
+
     if (absDiff >= 1 && absDiff <= 3) {
         if (!previousDirection) return 1;
         return (previousDirection === direction) ? 1 : 0;
