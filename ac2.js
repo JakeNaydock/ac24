019e605e8d3c5e352c2d42d42253b0420681ec71
Jake Naydock <jacob.naydock@quadratec.com>
2024-12-03 21:02:49 -0500

    Day 2 part 1 progress

--- ac2.js
+++ ac2.js
@@ -3,30 +3,45 @@ const fs = require('fs');
 const data = fs.readFileSync('./input2.txt', 'utf-8');
 const arrLines = data.split('\n');
 //console.log('arrLines:\n', arrLines);
-
+let safeRowCount = 0;
 for (let i = 0; i < arrLines.length; i++) {
     const arrCurLine = arrLines[i].split(' ');
     //console.log('arrCurLine:\n', arrCurLine);
 
-    let previousDirection = 'equal';
+    let previousDirection;
+    let direction;
     for (let j = 0; j < arrCurLine.length; j++) {
-
         const currentNum = arrCurLine[j] * 1;
-        const previousNum = (!j) ? null : arrCurLine[j - 1] * 1;
-        console.log(`Line ${i}. Current num: ${currentNum}. Previous num: ${previousNum}`);
-        if (!previousNum) continue;
+        //console.log('Arr curr line length', arrCurLine.length);
 
-        if (currentNum < previousNum) {
-            direction = 'decreasing';
-        } else if (currentNum === previousNum) {
-            direction = 'equal';
-        } else if (currentNum > previousNum) {
-            direction = 'increasing';
-        }
+        const previousNum = (j > 0) ? arrCurLine[j - 1] * 1 : null;
 
+        //Stop if nums are equal
 
-
-
+        //If there is no previous direction stop here
+        //if (!previousDirection) continue;
+        direction = (previousNum && currentNum < previousNum) ? 'decreasing' : 'increasing';
+        let intSafe = safetyCheck(currentNum, previousNum, direction, previousDirection);
+        console.log(`Safety check line ${i} char ${j}`, intSafe);
+        if (!intSafe) break;
+        if (j === arrCurLine.length - 1) safeRowCount++;
+        direction = previousDirection;
     }
+}
 
+console.log('Safe row count:', safeRowCount);
+
+
+function safetyCheck(currentNum, previousNum, direction, previousDirection) {
+    console.log(`Current num: ${currentNum}. Previous num: ${previousNum}`);
+    if (!previousNum) return 1;
+    if (currentNum === previousNum) return 0;
+    let absDiff = Math.abs(currentNum - previousNum);
+    console.log('Abs diff: ', absDiff);
+    if (absDiff >= 1 && absDiff <= 3) {
+        if (!previousDirection) return 1;
+        return (previousDirection === direction) ? 1 : 0;
+    } else {
+        return 0
+    }
 }
