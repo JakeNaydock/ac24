87941e5ff56d348df6fb175e7bbd54f9ab5b865b
Jake Naydock <jacob.naydock@quadratec.com>
2024-12-04 21:47:48 -0500

    Part 2 in progress

--- ac2.js
+++ ac2.js
@@ -1,26 +1,44 @@
 const fs = require('fs');
 const data = fs.readFileSync('./input2.txt', 'utf-8');
 const arrLines = data.split('\n');
+//console.log('Arrlines', arrLines);
 let safeRowCount = 0;
 for (let i = 0; i < arrLines.length; i++) {
     const arrCurLine = arrLines[i].split(' ');
     let previousDirection;
     let direction;
+
+
     for (let j = 0; j < arrCurLine.length; j++) {
         const currentNum = arrCurLine[j] * 1;
         const previousNum = (j > 0) ? arrCurLine[j - 1] * 1 : null;
+        let nextNum = (j < arrCurLine.length - 1) ? arrCurLine[j + 1] : null;
+        //console.log('Next num', nextNum);
         if (!previousNum) continue;
         direction = (currentNum < previousNum) ? 'decreasing' : 'increasing';
         let intSafe = safetyCheck(currentNum, previousNum, direction, previousDirection);
-        if (!intSafe) break;
+        let safeAfterRemoval;
+        if (!intSafe) {
+            safeAfterRemoval = safetyCheck(nextNum, previousNum, direction, previousDirection, true);
+            if (!safeAfterRemoval) break;
+        }
+        if (safeAfterRemoval) console.log(`Line ${i + 1} of text document, number ${j + 1} (${currentNum}) is safe after removal`);
         if (j === arrCurLine.length - 1) safeRowCount++;
+        if (safeAfterRemoval) {
+            console.log(`Line ${i + 1} of text document is safe, and contains a removal`);
+
+        }
+
+
         previousDirection = direction;
     }
 }
 
-console.log('Part 1 Answer:', safeRowCount);
+console.log('Part 2 Answer:', safeRowCount);
 
-function safetyCheck(currentNum, previousNum, direction, previousDirection) {
+function safetyCheck(currentNum, previousNum, direction, previousDirection, debug) {
+    if (debug) console.log(`Current (or next) num: ${currentNum}, Previous Num: ${previousNum}, Direction: ${direction}, Previous Direction: ${previousDirection}`);
+
     if (!previousNum) return 1;
     if (currentNum === previousNum) return 0;
     let absDiff = Math.abs(currentNum - previousNum);
