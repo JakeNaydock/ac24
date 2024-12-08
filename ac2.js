85642c41bdb35ea7eaf335bc189181dec7f75a1c
Jake <jnaydockeahs@gmail.com>
2024-12-08 16:53:48 -0500

    Finally got day 2 part 2 answer, day 2 complete

--- ac2.js
+++ ac2.js
@@ -1,54 +1,59 @@
 const fs = require('fs');
-const data = fs.readFileSync('./input2min.txt', 'utf-8');
+const data = fs.readFileSync('./input2.txt', 'utf-8');
 const arrLines = data.split('\n');
-//console.log('Arrlines', arrLines);
 let safeRowCount = 0;
+let unsafeLines = [];
 for (let i = 0; i < arrLines.length; i++) {
     const arrCurLine = arrLines[i].split(' ');
     let previousDirection;
     let direction;
-
-    let lineContainsRemoval = false;
     for (let j = 0; j < arrCurLine.length; j++) {
         const currentNum = arrCurLine[j] * 1;
         const previousNum = arrCurLine[j - 1];
-        let nextNum = arrCurLine[j + 1];
-        //console.log('Next num', nextNum);
-        //console.log('Next num', nextNum);
         if (!previousNum) continue;
         direction = (currentNum < previousNum) ? 'decreasing' : 'increasing';
         let intSafe = safetyCheck(currentNum, previousNum, direction, previousDirection);
-        console.log(`Index ${j} / Int Safe?: ${intSafe} Current num: ${currentNum}, Previous Num: ${previousNum}, Direction: ${direction}, Previous Direction: ${previousDirection}`);
-        let safeAfterRemoval;
         if (!intSafe) {
-            safeAfterRemoval = safetyCheck(nextNum, previousNum, direction, previousDirection, true);
-            console.log(`Safe after removal? ${safeAfterRemoval} (Index ${j} / Current num: ${currentNum})`);
-            if (!safeAfterRemoval) break;
-            lineContainsRemoval = true;
-            arrCurLine.splice(j, 1);
-            console.log(`Line ${i + 1} of text document, number ${j + 1} (${currentNum}) is safe after removal`);
-            //j = j - 1;
-            console.log('New j value before continuing loop: ', j);
+            unsafeLines.push(arrCurLine);
+            break;
         }
-        //!arrCurLine[j + 1]
-        //j === arrCurLine.length - 1
-        if (!nextNum) {
-            safeRowCount++;
-            console.log(`Line ${i + 1} of text document is safe, added to safeRow count (${safeRowCount})`);
-            if (lineContainsRemoval) {
-                console.log(`Line ${i + 1} of text document is safe, and contains a removal. Index ${j}`);
-            }
-        }
-
+        if (j === arrCurLine.length - 1) safeRowCount++;
         previousDirection = direction;
     }
 }
 
-console.log('Part 2 Answer:', safeRowCount);
+console.log('Part 1 Answer:', safeRowCount);
 
-function safetyCheck(currentNum, previousNum, direction, previousDirection, debug) {
-    if (debug) console.log(`Current (or next) num: ${currentNum}, Previous Num: ${previousNum}, Direction: ${direction}, Previous Direction: ${previousDirection}`);
+let safeWithRemovalRowCount = 0;
 
+for (let k = 0; k < unsafeLines.length; k++) {
+    let arrUnsafeLine = unsafeLines[k];
+    for (let m = 0; m < arrUnsafeLine.length; m++) {
+        let splicedArr = arrUnsafeLine.toSpliced(m, 1);
+        let previousDirection;
+        let direction;
+        let lineSafe = false;
+        for (let n = 0; n < splicedArr.length; n++) {
+            const currentNum = splicedArr[n] * 1;
+            const previousNum = splicedArr[n - 1] * 1;
+            if (!previousNum) continue;
+            direction = (currentNum < previousNum) ? 'decreasing' : 'increasing';
+            let intSafe = safetyCheck(currentNum, previousNum, direction, previousDirection);
+            if (!intSafe) break;
+            if (n === splicedArr.length - 1) {
+                safeWithRemovalRowCount++;
+                lineSafe = true;
+            }
+            previousDirection = direction;
+        }
+        if (lineSafe) break;
+    }
+}
+
+let combinedTotal = safeRowCount + safeWithRemovalRowCount;
+console.log('Part 2 answer? ', combinedTotal);
+
+function safetyCheck(currentNum, previousNum, direction, previousDirection) {
     if (!previousNum) return 1;
     if (currentNum === previousNum) return 0;
     let absDiff = Math.abs(currentNum - previousNum);
