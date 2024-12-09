fc8d5f656f46ea26115367f9b4625eec9fe8bff8
Jake Naydock <jacob.naydock@quadratec.com>
2024-12-08 19:44:22 -0500

    Day 3 part 1 complete

--- ac3.js
+++ ac3.js
@@ -1,38 +1,20 @@
-const { log } = require('console');
 const fs = require('fs');
-const data = fs.readFileSync('./input3min.txt', 'utf-8');
+const data = fs.readFileSync('./input3.txt', 'utf-8');
 const multiply = (num1, num2) => num1 * num2;
-//console.log('Data: ', data);
 
-//const re = /(mul)/g;
-//let reString = re.exec(data);
-//console.log('Restring: ', reString);
 
-//const firstIndex = data.indexOf('mul(');
-//let newStr = data.slice(firstIndex, firstIndex + 12);
-//console.log(`newStr: `, newStr);
-
-let numbersToMultiply = [];
-let validSequence = false;
-let capturingNumbers = false;
-let stringBuilder = '';
-
-for (let i = 0; i < data.length; i++) {
-    const currentChar = data[i];
-    console.log(`Current i value: `, i);
-    if (currentChar === 'm') {
-        console.log(`Current char: `, currentChar);
-        let slicedData = data.slice(i, i + 4);
-        console.log(`Sliced Data: `, slicedData);
-        console.log(`i + 5: `, `i + 5 val: ${i + 5}, char for i + 5: ${data[i + 5]}`);
-        if (slicedData === 'mul(') {
-            validSequence = true;
-            i = i + 3;
-            continue;
-        }
+function findAllMulStrings(str) {
+    const regex = /mul\((\d{1,3}),\s?(\d{1,3})\)/g; // Notice the 'g' flag for global matching
+    let matches = [];
+    let match;
+    while ((match = regex.exec(str)) != null) {
+        matches.push([parseInt(match[1]), parseInt(match[2])]); // Convert numbers to integers
     }
-    if (validSequence) console.log(`In valid sequence, current char / previous char: `, currentChar + '/' + data[i - 1]);
-    if (validSequence) {
-        if (!isNaN(currentChar)) capturingNumbers = true;
-    }
+    return matches;
 }
+
+let arrNumbersToMultiply = findAllMulStrings(data);
+
+let sum = 0;
+arrNumbersToMultiply.forEach((el) => sum += multiply(el[0], el[1]));
+console.log(`Part 1 total: `, sum);
