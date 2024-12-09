01f18630b7e40dd54bca19a6954c6d4a9da01db6
Jake Naydock <jacob.naydock@quadratec.com>
2024-12-08 20:55:22 -0500

    Day 3 part 2 progress

--- ac3.js
+++ ac3.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
-const data = fs.readFileSync('./input3.txt', 'utf-8');
+const { type } = require('os');
+const { domainToASCII } = require('url');
+const data = fs.readFileSync('./input3min.txt', 'utf-8');
+
 const multiply = (num1, num2) => num1 * num2;
+const between = (num, min, max) => num > min && num < max;
 
 
 function findAllMulStrings(str) {
@@ -18,3 +22,57 @@ let arrNumbersToMultiply = findAllMulStrings(data);
 let sum = 0;
 arrNumbersToMultiply.forEach((el) => sum += multiply(el[0], el[1]));
 console.log(`Part 1 total: `, sum);
+
+//Starting part 2 below:
+const doMatchRe = /(do\(\))/g;
+const dontMatchRe = /(don\'t\(\))/g;
+let doMatches = Array.from(data.matchAll((doMatchRe)), (m) => m.index);
+let dontMatches = Array.from(data.matchAll(dontMatchRe), (m) => m.index);
+console.log(`Do matches: `, doMatches);
+console.log(`Don't matches `, dontMatches);
+
+console.log(`Between test: `, between(6, 5, 10));
+console.log(`Merge sorted arrays: `, mergeSortedArrays(doMatches, dontMatches));
+
+
+function mergeSortedArrays(arr1, arr2) {
+    const mergedArray = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergedArray.push({
+                type: 1,
+                index: arr1[i]
+            });
+            i++;
+        } else {
+            mergedArray.push({
+                type: 0,
+                index: arr2[j]
+            });
+            j++;
+        }
+    }
+
+    // Add any remaining elements from arr1
+    while (i < arr1.length) {
+        mergedArray.push({
+            type: 1,
+            index: arr1[i]
+        });
+        i++;
+    }
+
+    // Add any remaining elements from arr2
+    while (j < arr2.length) {
+        mergedArray.push({
+            type: 0,
+            index: arr2[j]
+        });
+        j++;
+    }
+
+    return mergedArray;
+}
