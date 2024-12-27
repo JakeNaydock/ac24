6fd3c452f6a82ba2eb40452300c3c49bb22cb159
Jake Naydock <jacob.naydock@quadratec.com>
2024-12-26 21:37:20 -0500

    Day 5 p1 progress

--- ac5.js
+++ ac5.js
@@ -3,14 +3,33 @@ const data = fs.readFileSync('./input5.txt', 'utf-8');
 
 const splitData = data.split('\n\n');
 //console.log(`Splitdata: `, splitData);
-const rules = splitData[0].split('\n').map((rule) => rule.split('|'));
+const splitToNumber = (row, delimiter) => {
+    row = row.split(delimiter);
+    return row.map((str) => str * 1);
+}
+const rules = splitData[0].split('\n').map((rule) => splitToNumber(rule, '|'));
 console.log(`rules: `, rules);
-const instructions = splitData[1].split('\n').map((row) => row.split(','));
+
+const instructions = splitData[1].split('\n').map((row) => splitToNumber(row, ','));
 console.log(`instructions: `, instructions);
 
 /**
  * Loop through each instruction line
  * Loop through each instruction on a given line
  * For each instruction, check where that appears in the list of rules
- * For each of those rules, compare to the
+ * For each of those rules, find the other number in the rule.
+ * Check for that number in the instruction line, and see if the number comes before or after according to the rule
+ * If rule passes, move to next rule until are rules are complete.
+ * If all rules pass, move to next number in that instruction
+ * If all numbers in that line pass all rules - mark line as passed
+ * Find middle number of the line that passes
+ * Add that number to total sum
  */
+
+for (let i = 0; i < instructions.length; i++) {
+    let instructionLine = instructions[i];
+    for (let j = 0; j < instructionLine.length; j++) {
+
+    }
+
+}
