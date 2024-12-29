eeefc4c8482d6d383eb0886bdc7292988f6b7a4e
Jake Naydock <jacob.naydock@quadratec.com>
2024-12-29 17:51:12 -0500

    Working on day 5 p2

--- ac5-2.js
+++ ac5-2.js
@@ -9,6 +9,21 @@ const splitToNumber = (row, delimiter) => {
 const rules = splitData[0].split('\n').map((rule) => splitToNumber(rule, '|'));
 const instructions = splitData[1].split('\n').map((row) => splitToNumber(row, ','));
 
+const ruleMap = new Map();
+/*
+ruleMap.set(47, [82, 58, 91, 11, 23]);
+console.log(`Rulemap get: `, ruleMap.get(47));
+ruleMap.set(ruleMap.get(47).push(55));
+console.log(`Rulemap get agaiin: `, ruleMap.get(47));
+*/
+for (const [rule1, rule2] of rules) {
+    if (ruleMap.has(rule1)) {
+        ruleMap.set(ruleMap.get(rule1).push(rule2));
+    } else {
+        ruleMap.set(rule1, [rule2]);
+    }
+}
+console.log(`Rule map: `, ruleMap);
 
 for (let i = 0; i < instructions.length; i++) {
     const instructionLine = instructions[i];
@@ -22,6 +37,7 @@ for (let i = 0; i < instructions.length; i++) {
             if (currentNum === rule1) {
                 if (instructionLine.indexOf(rule2) !== -1 && (instructionLine.indexOf(currentNum) > instructionLine.indexOf(rule2))) {
                     charPassesRules = false;
+                    instructionLine.splice();
                     break;
                 }
             } else {
