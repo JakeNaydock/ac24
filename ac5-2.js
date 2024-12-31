6edeb972226c5e21bd0a9129ff23ff4cb4fe74b0
Jake Naydock <jacob.naydock@quadratec.com>
2024-12-31 10:23:20 -0500

    day 5 p2

--- ac5-2.js
+++ ac5-2.js
@@ -10,19 +10,16 @@ const rules = splitData[0].split('\n').map((rule) => splitToNumber(rule, '|'));
 const instructions = splitData[1].split('\n').map((row) => splitToNumber(row, ','));
 
 const ruleMap = new Map();
-/*
-ruleMap.set(47, [82, 58, 91, 11, 23]);
-console.log(`Rulemap get: `, ruleMap.get(47));
-ruleMap.set(ruleMap.get(47).push(55));
-console.log(`Rulemap get agaiin: `, ruleMap.get(47));
-*/
+
 for (const [rule1, rule2] of rules) {
     console.log(`Rulemap has ${rule1}: `, ruleMap.has(rule1));
     console.log(`Typeof rule 1: `, typeof (rule1));
     //if (ruleMap.get(rule1) === undefined) continue;
-    if (ruleMap.has(rule1) || ruleMap.get(rule1) != undefined) {
+    if (ruleMap.has(rule1)) {
         console.log(`rowmap get rule1 ${rule1}: `, ruleMap.get(rule1));
-        ruleMap.set(ruleMap.get(rule1).push(rule2));
+        let currentArr = ruleMap.get(rule1);
+        currentArr.push(rule2);
+        ruleMap.set(rule1, currentArr);
     } else {
         ruleMap.set(rule1, [rule2]);
     }
@@ -31,32 +28,31 @@ for (const [key, value] of ruleMap.entries()) {
     console.log(`${key} = ${value}`);
 }
 
-/*
+//console.log(`Rule map: `, ruleMap);
+
+/**
+ * Loop through each instruction line
+ * For each number, get rules from the rule map
+ * For each rule in the map for that number, check the index of the number in the current instruction
+ * No matter what - the number will need to come before the LOWEST index
+ * Take note of the lowest index per number on that instruction line, push that into a new map
+ * Iterate through that map and rebuild instruction line in order of lowest index least to greatest
+ * Take middle num and add it to the sum
+ */
+
+
 for (let i = 0; i < instructions.length; i++) {
     const instructionLine = instructions[i];
 
     for (let j = 0; j < instructionLine.length; j++) {
         const currentNum = instructionLine[j];
-        const matchingRules = rules.filter(subArray => subArray.includes(currentNum));
+
         let charPassesRules = true;
 
-        for (const [rule1, rule2] of matchingRules) {
-            if (currentNum === rule1) {
-                if (instructionLine.indexOf(rule2) !== -1 && (instructionLine.indexOf(currentNum) > instructionLine.indexOf(rule2))) {
-                    charPassesRules = false;
-                    instructionLine.splice();
-                    break;
-                }
-            } else {
-                if (instructionLine.indexOf(rule1) !== -1 && (instructionLine.indexOf(currentNum) < instructionLine.indexOf(rule1))) {
-                    charPassesRules = false;
-                    break;
-                }
-            }
-        }
+        let ruleArr = ruleMap.get(currentNum);
+        let lowestIndex = null;
 
         if (charPassesRules === false) break;
     }
 }
 
-*/
